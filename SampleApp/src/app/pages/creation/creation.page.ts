191aa98b2218e9edef14e910d565914a8f9da2d6
Yu-Hsuan Huang <yuhsuan.huang.tw@gmail.com>
2021-08-12 22:20:19 -0400

    Upload Ionic sample code

---
+++ SampleApp/src/app/pages/creation/creation.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-creation',
+  templateUrl: './creation.page.html',
+  styleUrls: ['./creation.page.scss'],
+})
+export class CreationPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
